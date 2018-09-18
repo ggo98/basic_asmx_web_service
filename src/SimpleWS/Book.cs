05c4184d0f655250cf016ed22d94288d66aa7268
ggo <ggo@clickndecide.com>
2018-09-18 16:37:50 +0200

    initial commit

---
+++ src/SimpleWS/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleWS
+{
+	public class Book
+	{
+		private static Book[] _mockData =
+		{
+			new Book() { Name = "Book 1", Year = 2011 },
+			new Book() { Name = "Book 2", Year = 2012 },
+			new Book() { Name = "Book 3", Year = 2013 },
+		};
+
+		public static Book[] MockData 
+		{ 
+			get { return _mockData; }
+		}
+
+		public string Name { get; set; }
+		public int Year { get; set; }
+	}
+}
---
+++ src/SimpleWS/Service.asmx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SimpleWS
+{
+	/// <summary>
+	/// Summary description for Service
+	/// </summary>
+	[WebService(Namespace = "http://tempuri.org/")]
+	[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+	[System.ComponentModel.ToolboxItem(false)]
+	// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+	// [System.Web.Script.Services.ScriptService]
+	public class Service : System.Web.Services.WebService
+	{
+
+		[WebMethod]
+		public int BookYear(Book book)
+		{
+			if (null != book)
+			{
+				var b = (from data in Book.MockData
+						 where data.Name.Equals(book.Name, StringComparison.InvariantCultureIgnoreCase)
+						 select data).FirstOrDefault();
+				if (null != b)
+					return b.Year;
+			}
+			return -1;
+		}
+	}
+}

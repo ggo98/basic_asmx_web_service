332c8d710ed3fddfe543cc480300e70887ef3be6
ggo <ggo@clickndecide.com>
2023-05-03 18:26:13 +0200

    VERSION 2.0

--- src/SimpleWS/Service.asmx.cs
+++ src/SimpleWS/Service.asmx.cs
@@ -16,7 +16,7 @@ namespace SimpleWS
 	// [System.Web.Script.Services.ScriptService]
 	public class Service : System.Web.Services.WebService
 	{
-		// VERSION 1.0
+		// VERSION 2.0
 
 		void maMethode()
 		{

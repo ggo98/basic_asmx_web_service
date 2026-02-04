504e3fc836c61b3e8301a3cbda2805672f758df2
O <ggo98@yahoo.com>
2026-02-04 16:32:54 +0100

    4-add-a-new-book

---
+++ src/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.ServiceReference1;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Book svc = new ServiceReference1.Book();
+            //BookYearRequest req = new BookYearRequest() { Body = new BookYearRequestBody() { book = new Book(
+
+            ServiceSoapClient clt = new ServiceSoapClient();
+            Book b = new Book() { Name = "test 4" };
+            var year = clt.BookYear(b);
+
+            //svc.BookYear(
+            //Client.ServiceReference1 service = new 
+        }
+    }
+}
